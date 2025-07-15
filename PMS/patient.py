e834c4ee2440994b5c55a3237fc244fe6cd0dbcd
Abby Maina <164876323+abbymaina@users.noreply.github.com>
2025-07-15 10:57:39 +0300

    Updated

--- PMS/patient.py
+++ PMS/patient.py
@@ -1,9 +1,9 @@
 class Patient:
-    def _init_(self, patient_id, name, age, gender):
+    def __init__(self, patient_id, name, age, gender):
         self.id = patient_id
         self.name = name
         self.age = age
         self.gender = gender
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.id}: {self.name}, {self.age}, {self.gender}"
