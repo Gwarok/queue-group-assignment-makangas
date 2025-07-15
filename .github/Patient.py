6127b9dc244f4c442d82ef81e7457989078d6bbd
Gwarok <gwarokebiro@gmail.com>
2025-07-15 09:40:47 +0300

    moved patient.py to correct spot

--- .github/Patient.py
+++
@@ -1,9 +0,0 @@
-class Patient:
-    def _init_(self, patient_id, name, age, gender):
-        self.id = patient_id
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-    def _str_(self):
-        return f"{self.id}: {self.name}, {self.age}, {self.gender}"
